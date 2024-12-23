aa5a6a019fd3bd2fb658957860f898fc7d6665fa
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2024-12-23 18:30:57 +0100

    added P18 from CAV13

--- src/bisimulation_learning/fintely_branching/experiments/nd_term.py
+++ src/bisimulation_learning/fintely_branching/experiments/nd_term.py
@@ -133,6 +133,55 @@ def P17():
 
     return transition_system, template
 
+
+"""
+P18 from [Beyne et al. 2013]
+See T2 repo.
+
+"""
+def successors_P18(s):
+    succ_s_act_1 = [v for v in s] # if random = false
+    succ_s_act_2 = [v for v in s] # if random = true
+
+    succ_s_act_1[0] = s[0] + 1
+
+    succ_s_act_2[0] = If(s[0] > 2, 
+        2, 
+        s[0]
+    )
+
+    return [succ_s_act_1, succ_s_act_2]
+
+# BDT Template - P18
+# 
+# Labelling Function: x[0] >= 1, x[0] < 1
+def bdt_P18(params, x, num_params, partitions):
+        b = BDTNodePoly([RealVal(1)],x, RealVal(5), 
+                BDTNodePoly([params[num_params]], x, params[0],
+                                BDTLeave(partitions[0]), BDTLeave(partitions[1])),
+                BDTNodePoly([params[num_params+1]], x, params[1],
+                                BDTLeave(partitions[2]), BDTLeave(partitions[3]))
+                )
+        return b.formula(), b
+def P18():
+    dim = 1
+
+    transition_system = BranchingTransitionSystem(
+        dim,
+        successors = successors_P18
+    )
+
+
+    template = BDTTemplate(
+        dim=dim,
+        bdt_classifier=bdt_P18,
+        num_coefficients=2,
+        num_params=2,
+        num_partitions=4
+    )
+
+    return transition_system, template
+
 """
 Branching system termination (2)
 
