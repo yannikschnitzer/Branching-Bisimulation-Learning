25afde9bf5a4b10104123c0ac2aa001f9bb2480c
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2024-12-27 10:51:35 +0100

    update

--- src/run_branching.py
+++ src/run_branching.py
@@ -9,7 +9,7 @@ def run_and_show(trs, tem):
     draw_quotient(gamma, "Default Name")
     visualize_branching(theta, tem)
 
-def run_and_print(trs, tem: BDTTemplate, expl: False):
+def run_and_print(trs, tem: BDTTemplate, expl: True):
     theta, eta = bisimulation_learning(trs, tem, iters=100, explicit_classes=expl)
     print(f"""
     theta = {theta},
@@ -42,5 +42,5 @@ if __name__ == "__main__":
     # trs, tem = term_loop_nd_y()
     # run_and_show(trs, tem)
     #trs, tem = robots()
-    run_and_print(trs, tem, expl = False)
+    run_and_print(trs, tem, expl = True)
     
