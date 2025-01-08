2e26ad301027d645e037a5fd812dd11de6487ca4
Christian Micheletti <christian.micheletti@studenti.unipd.it>
2025-01-08 19:56:17 +0000

    wip benchmarks

--- src/t2-prover/src/benchmarks.py
+++ src/t2-prover/src/benchmarks.py
@@ -5,7 +5,8 @@ import re
 import os
 import numpy as np
 
-# mono src/bin/Debug/T2.exe term_loop_nd_2.T2 --CTL '[EG]([AF](varX == 0))'
+# mono src/bin/Debug/T2.exe bl_tests/term_loop_nd_2.t2 --CTL '[EG]([AF](varX == 0))'
+# mono src/bin/Debug/T2.exe bl_tests/term_loop_nd_2.t2 --CTLStar 'G(F(varX == 0))'
 
 experiments = [
     {
@@ -106,7 +107,7 @@ experiments = [
     },
     {
         'name': "test/cav13-ctl-examples/P25.t2",
-        'formula': "(varC <= 5) || ([AF](varR > 5))"
+        'formula': "(varC > 5) -> ([AF](varR > 5))"
     },
     {
         'name': "test/cav13-ctl-examples/P26.t2",
@@ -134,14 +135,13 @@ experiments = [
     }
 ]
 
-def run_t2_experiment(exp, negated=False):
+def run_t2_experiment(exp, ltl=False):
     formula = exp['formula']
-    if negated:
-        formula = f"!({formula})"
+    star = 'Star' if ltl else ''
     cmd = f"""
     mono src/bin/Debug/T2.exe \\
         {exp['name']} \\
-        --CTL '{formula}' 
+        --CTL{star} '{formula}' 
     """
     outb = subprocess.check_output(cmd, shell=True, stderr=subprocess.DEVNULL)
     out = outb.decode("utf-8")
@@ -150,20 +150,25 @@ def run_t2_experiment(exp, negated=False):
         print("Alarm! Alarm!")
         raise Exception(out)
 
-def measure_t2_experiment(exp, negated=False):
-    # retry until success
+def measure_t2_experiment(exp, ltl):
     start_time = time.time()
-    run_t2_experiment(exp, negated)
+    run_t2_experiment(exp, ltl)
     stop_time = time.time()
     return start_time, stop_time
 
 
-def run_t2_experiments(iters, verbose=False, negated=False):
+def run_t2_experiments(iters, verbose=False, negated=False, ltl=False):
     for exp in experiments:
+        formula = exp['formula']
+        if negated:
+            formula = f"!({formula})"
+        if ltl:
+            formula = re.sub(r"\[[AE](.)\]", r"\1", formula)
+        exp['formula'] = formula
         times = []
         try:
             for i in range(iters):
-                start_time, stop_time = measure_t2_experiment(exp, negated)
+                start_time, stop_time = measure_t2_experiment(exp, ltl)
                 times.append(stop_time - start_time)
                 if verbose:
                     print(f"--- Experiment {exp} - {i}th run expired in {times[-1]}s")
@@ -184,12 +189,14 @@ if __name__ == "__main__":
     )
     arg_parser.add_argument("-i", "--iters")
     arg_parser.add_argument("-n", "--negated", action="store_true")
+    arg_parser.add_argument("--ltl", action="store_true")
     arg_parser.add_argument("-v", "--verbose", action="store_true")
     args = arg_parser.parse_args()
     
     iters = args.iters or 10 # default iterations is 10
     iters = int(iters)
     negated = args.negated
+    ltl     = args.ltl
     verbose = args.verbose
     os.system("")
-    run_t2_experiments(iters, verbose=verbose, negated=negated)
+    run_t2_experiments(iters, verbose=verbose, negated=negated, ltl=ltl)
