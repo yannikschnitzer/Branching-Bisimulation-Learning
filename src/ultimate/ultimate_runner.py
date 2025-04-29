6e3d5aebf5b6e65cceaa2a3a81287e69f4f88ccb
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2025-04-29 09:59:00 +0200

    updated ultimate java version switching

--- src/ultimate/ultimate_runner.py
+++ src/ultimate/ultimate_runner.py
@@ -19,29 +19,29 @@ class Ultimate_Experiment:
         self.file = file
         self.print_res = print_res
 
-def run_ultimate_experiment(exp : Ultimate_Experiment):
-        """
-            Runs Ultimate with given experiment.
-        """
-        cmd = "sudo update-java-alternatives --set java-1.21.0-openjdk-amd64 && ../../ultimate/releaseScripts/default/UAutomizer-linux/Ultimate -tc ../../ultimate/releaseScripts/default/UAutomizer-linux/config/AutomizerTermination.xml -i ../C-Programs/" + exp.file
-        os.system("")
-        print("------------------------------------")
-        print("Running Experiment: ", exp.name)
-        out = subprocess.check_output(cmd, shell = True, text = True, stderr=subprocess.DEVNULL)
-        time_pattern = r"Automizer plugin needed (\d+\.\d+)"
-        matches = re.findall(time_pattern, out)
-        if matches:
-            print("Ultimate Automizer Analysis Time:", matches[0],"")
-            return float(matches[0])
-        else:
-            raise Exception("No match")
+# def run_ultimate_experiment(exp : Ultimate_Experiment):
+#         """
+#             Runs Ultimate with given experiment.
+#         """
+#         cmd = "sudo update-java-alternatives --set java-1.21.0-openjdk-amd64 && ../../ultimate/releaseScripts/default/UAutomizer-linux/Ultimate -tc ../../ultimate/releaseScripts/default/UAutomizer-linux/config/AutomizerTermination.xml -i ../C-Programs/" + exp.file
+#         os.system("")
+#         print("------------------------------------")
+#         print("Running Experiment: ", exp.name)
+#         out = subprocess.check_output(cmd, shell = True, text = True, stderr=subprocess.DEVNULL)
+#         time_pattern = r"Automizer plugin needed (\d+\.\d+)"
+#         matches = re.findall(time_pattern, out)
+#         if matches:
+#             print("Ultimate Automizer Analysis Time:", matches[0],"")
+#             return float(matches[0])
+#         else:
+#             raise Exception("No match")
 
-        if exp.print_res:
-            res_pattern = r"TerminationAnalysisResult: (.*)"
-            matches = re.findall(res_pattern, out)
-            if matches:
-                print("Verification result:", matches[0])
-        print("------------------------------------")
+#         if exp.print_res:
+#             res_pattern = r"TerminationAnalysisResult: (.*)"
+#             matches = re.findall(res_pattern, out)
+#             if matches:
+#                 print("Verification result:", matches[0])
+#         print("------------------------------------")
 
 def run_ultimate_experiment(exp: Ultimate_Experiment) -> float:
     """
